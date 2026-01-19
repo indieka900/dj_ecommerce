ae14a91d9be441b530fdd8845c15e1952c6acb6d
Joseph Indieka💻 <indiekaj@gmail.com>
2026-01-19 19:44:07 +0300

    load data file

--- build.sh
+++ build.sh
@@ -12,5 +12,7 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input --settings=ecommerce_proj.settings.prod
 python manage.py migrate --settings=ecommerce_proj.settings.prod
 
+echo "Uploading json data"
+
 # import data.json file
-python manage.py loaddata data.json
+python manage.py loaddata data.json --settings=ecommerce_proj.settings.prod
